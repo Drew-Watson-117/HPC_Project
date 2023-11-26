dc0de3c9cf1661365ad859f074ac3227d12b00f5
Chandler Justice <chandler@cock.li>
2023-11-26 13:49:00 -0700

    MPI implementation complete

--- src/cpu_distributed/cpu_distributed.cpp
+++ src/cpu_distributed/cpu_distributed.cpp
@@ -1,50 +1,33 @@
 #include "helper_functions.hpp"
 #include <algorithm>
 #include <chrono>
-#include <mpi.h>
 #include <stdio.h>
+#include <mpi.h>
 /*  
     Given an elevation map, a starting coordinate, and an ending coordinate, count the number of pixels the two points which have line of sight with the starting point.
     May specify x dimension of the elevation map xDim and whether to display debug messages
 
     compile w/o build tool: mpic++ -g -Wall -o cpu_distributed cpu_distributed.cpp helper_functions.hpp helper_functions.cpp
-    run: mpiexec ./cpu_distributed
+    run: mpiexec -n [process count] ./cpu_distributed
 */
 int countLineOfSight(std::vector<int16_t> &data, int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t xDim = 6000, bool debug = false);
-// Implementation of countLineOfSight using shorts instead of doubles for all z computation
-int countLineOfSightInt(std::vector<int16_t> data, int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t xDim = 6000, bool debug = false);
 
-/*
-    Functions for testing countLineOfSight on a dataset
-*/
-void test(bool debug=false);
-// Tests an iteration of the algorithm with a specified range (radius to consider)
-void timing(int16_t range=100);
 
-/*
-	Function which contains the logic for the serial implementation of the algorithm
-*/
-//void serial(int16_t range=100);
+void MPI_line_of_sight(std::vector<int16_t> &data, std::vector<int> &counts, int16_t range, int my_rank, int comm_sz, int data_split, int data_overflow);
 
 
 //global communication
 MPI_Comm comm;
-int main(int argc, char* argv[]) { 
-    
-   
 
+const int DATA_COUNT = 6000 * 6000; 
+
+int main(int argc, char* argv[]) { 
     //initialize MPI
     MPI_Init(&argc, &argv);
-
-
-    int data_count = 6000 * 6000; 
-
-
-    std::vector<int16_t> data_t;
-    int16_t* data = new int16_t[data_count];
-    std::vector<int> counts(6000*6000, 0);
+    
     int my_rank;
     int comm_sz;
+   
     //get number of processes available
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
@@ -53,64 +36,120 @@ int main(int argc, char* argv[]) {
 
     //communication channel
     comm = MPI_COMM_WORLD;
-    
+
+
+    std::vector<int16_t> data(DATA_COUNT);
+    std::vector<int> global_counts(DATA_COUNT);
+
+    int work_distribution[comm_sz];
+    int displacements[comm_sz];
+
+    int data_overflow = DATA_COUNT % comm_sz;
+    int data_split = ((DATA_COUNT - data_overflow) / comm_sz);
+
     //rank 0 loads data and broadcasts it to all other threads
     if (my_rank == 0){
        std::cout << "getting data from file" << std::endl;
-       data_t = getData();
-       data = data_t.data();
-       std::cout << "previewing data element: " << data[0] << std::endl;
+       data = getData();
 
-       //MPI_Bcast(&data_count, 1, MPI_INT, 0, comm);
+       int curr_displacement = 0;
+       for(int i = 0; i < comm_sz; i++){
+           if(i == (comm_sz - 1)){
+               displacements[i] = curr_displacement;
+               work_distribution[i] = data_split + data_overflow;
+           }
+           else{
+               displacements[i] = curr_displacement;
+               work_distribution[i] = data_split;
+               curr_displacement += data_split;
+           }
+       }
     }
-    //broadcast vector size to all other threads
-    MPI_Bcast(&data[0], data_count, MPI_SHORT, 0, comm);
+
+    global_counts.resize(DATA_COUNT);
+    //all other ranks resize their data vector in anticipation of the incoming data
+    if (my_rank != 0){
+        data.resize(DATA_COUNT);
+    }
+   
+    //broadcast vector size & work distribution to all other threads
+    MPI_Bcast(data.data(), DATA_COUNT, MPI_SHORT, 0, comm);
+    MPI_Bcast(&work_distribution, comm_sz, MPI_INT, 0, comm);
+    MPI_Bcast(&displacements, comm_sz, MPI_INT, 0, comm);
+    std::vector<int> local_counts(work_distribution[my_rank]);
+    
+    MPI_line_of_sight(data, local_counts, 10, my_rank, comm_sz, data_split, data_overflow);
     MPI_Barrier(comm);
-    for(int i = 0; i < 10; i++){
-        printf("rank %i, item %i: %i\n", my_rank, i, data[i]);
-    }
+    
+    MPI_Gatherv(
+            &local_counts.front(),
+            local_counts.size(), 
+            MPI_INT, 
+            &global_counts.front(), 
+            work_distribution,
+            displacements,
+            MPI_INT, 
+            0, 
+            comm
+            );
 
+    if(my_rank == 0){
+        //cannot use writeFile from helperfunctions as freeing buffer causes seg. fault.
+        std::ofstream file;
+        file.open("cpu_distributed_out.raw", std::ios::binary);
+        if (file.is_open()) {
+            char* buffer = reinterpret_cast<char*>(global_counts.data());
+            size_t size = global_counts.size() * sizeof(int);
+            file.write(buffer, size);
+            file.close();
+        } 
+    }
     //shut down mpi
     MPI_Finalize(); 
+   
     return 0;
 }
 
-//void serial(int16_t range) {
-//	std::vector<int16_t> data = getData();
-//
-//    std::vector<int> counts(6000*6000,0);
-//
-//    // For each pixel
-//    for (int16_t centerX = 0; centerX < 6000; ++centerX) {
-//        for (int16_t centerY = 0; centerY < 6000; ++centerY) {
-//            int count = 0;
-//            // For each pixel on the edge of a square of radius 100 around the center pixel
-//            // std::max and std::min ensure considerX and considerY are in the bounds of image
-//
-//			// Fix considerX at centerX - 100, vary considerY
-//			for (short considerY = std::max(centerY - range, 0); considerY <= std::min(centerY + range,5999); ++considerY) {
-//				count += countLineOfSight(data,centerX,centerY,std::max(0,centerX - range),considerY);
-//			}
-//			// Fix considerX at centerX + 100, vary considerY
-//			for (short considerY = std::max(centerY - range, 0); considerY <= std::min(centerY + range,5999); ++considerY) {
-//				count += countLineOfSight(data,centerX,centerY,std::min(5999,centerX + range),considerY);
-//			}
-//			// Fix considerY at centerY - 100, vary considerX
-//			for (short considerX = std::max(centerX - range, 0); considerX <= std::min(centerX + range,5999); ++considerX) {
-//				count += countLineOfSight(data,centerX,centerY,considerX,std::min(5999,centerY + range));
-//			}
-//			// Fix considerY at centerY + 100, vary considerX
-//			for (short considerX = std::max(centerX - range, 0); considerX <= std::min(centerX + range,5999); ++considerX) {
-//				count += countLineOfSight(data,centerX,centerY,considerX,std::min(5999,centerY + range));
-//			}
-//            counts[getIndex(centerX,centerY)] = count;
-//        }
-//    }
-//
-//    writeFile(counts,"output.raw");
-//    std::cout << "Done!" << std::endl;
-//    std::cout << "Visible pixels from (0,0): " << counts[getIndex(0,0)] << std::endl; 
-//}
+void MPI_line_of_sight(std::vector<int16_t> &data, std::vector<int> &counts, int16_t range, int my_rank, int comm_sz, int data_split, int data_overflow){
+    int start_point, end_point; 
+    start_point = data_split * my_rank;
+    
+    if(my_rank == comm_sz - 1){ // if I am the last process, then I get some extra work if needed
+        end_point = start_point + data_split + data_overflow; 
+    }
+    else{
+        end_point = start_point + data_split;
+    }
+
+    for (int i = start_point; i < end_point; i++){
+        int centerX = i % 6000;
+        int centerY = i / 6000;
+        
+        int count = 0;
+        // For each pixel on the edge of a square of radius 100 around the center pixel
+        // std::max and std::min ensure considerX and considerY are in the bounds of image
+
+		// Fix considerX at centerX - 100, vary considerY
+		for (short considerY = std::max(centerY - range, 0); considerY <= std::min(centerY + range,5999); ++considerY) {
+			count += countLineOfSight(data,centerX,centerY,std::max(0,centerX - range),considerY);
+		}
+		// Fix considerX at centerX + 100, vary considerY
+		for (short considerY = std::max(centerY - range, 0); considerY <= std::min(centerY + range,5999); ++considerY) {
+			count += countLineOfSight(data,centerX,centerY,std::min(5999,centerX + range),considerY);
+		}
+		// Fix considerY at centerY - 100, vary considerX
+		for (short considerX = std::max(centerX - range, 0); considerX <= std::min(centerX + range,5999); ++considerX) {
+			count += countLineOfSight(data,centerX,centerY,considerX,std::min(5999,centerY + range));
+		}
+		// Fix considerY at centerY + 100, vary considerX
+		for (short considerX = std::max(centerX - range, 0); considerX <= std::min(centerX + range,5999); ++considerX) {
+			count += countLineOfSight(data,centerX,centerY,considerX,std::min(5999,centerY + range));
+		}
+        counts[i - start_point] = count;
+   }
+
+}
+
 
 int countLineOfSight(std::vector<int16_t> &data,int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t xDim, bool debug)
 {
@@ -295,40 +334,4 @@ int countLineOfSight(std::vector<int16_t> &data,int16_t x1, int16_t y1, int16_t 
     return count;
 }
 
-void test(bool debug) {
-    std::vector<int16_t> testData{0, 0, 0, 0,
-                                  1, 1, 1, 1,
-                                  2, 2, 3, 2,
-                                  3, 3, 3, 3};
-    int count;
 
-	int x1 = 0, y1 = 0;
-	int x2 = 0, y2 = 0;
-	std::cout << "Testing from (" << x1 << "," << y1 << ") to (" << x2 << "," << y2 << "): ";
-    count = countLineOfSight(testData,x1,y1,x2,y2,4,debug);
-    std::cout << count << std::endl;
-	x2 = 0, y2 = 3;
-	std::cout << "Testing from (" << x1 << "," << y1 << ") to (" << x2 << "," << y2 << "): ";
-    count = countLineOfSight(testData,x1,y1,x2,y2,4,debug);
-    std::cout << count << std::endl;
-	x2 = 3, y2 = 0;
-	std::cout << "Testing from (" << x1 << "," << y1 << ") to (" << x2 << "," << y2 << "): ";
-    count = countLineOfSight(testData,x1,y1,x2,y2,4,debug);
-    std::cout << count << std::endl;
-	x2 = 3, y2 = 3;
-	std::cout << "Testing from (" << x1 << "," << y1 << ") to (" << x2 << "," << y2 << "): ";
-    count = countLineOfSight(testData,x1,y1,x2,y2,4,debug);
-    std::cout << count << std::endl;
-
-}
-
-void timing(int16_t range) {
-	std::vector<int16_t> data = getData();
-	int count;
-	std::cout << "Timing double implementation" << std::endl;
-	auto start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < 100; i++) count = countLineOfSight(data,200,200,200,200-range);
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop-start);
-	std::cout << duration.count() / 100 << " us" << std::endl;
-}
